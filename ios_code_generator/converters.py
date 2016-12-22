d416cb2ec8f533125ea5736d58cae9d7d8500ebd
banxi1988 <banxi1988@gmail.com>
2016-12-22 17:41:40 +0800

    修正 json bool 类型的判断. 由于 bool 也是 int 所以先判断 bool

--- ios_code_generator/converters.py
+++ ios_code_generator/converters.py
@@ -27,14 +27,15 @@ class JSONField(object):
         if isinstance(self.value,(str,unicode)) :
             if self.value.startswith('http'):return 'u'
             elif self.value.startswith('is'): return 'b'
+        elif isinstance(self.value, bool):
+            return 'b'
         elif isinstance(self.value, (int,long)):
             has_di_name = any(dn in self.name for dn in ['create', 'update','modi','delete', 'time'])
             if self.value > 946656000 and has_di_name: return 'di'
             else:return 'i'
         elif isinstance(self.value, float):
             return 'd'
-        elif isinstance(self.value, bool):
-            return 'b'
+
         return ''
 
     def __str__(self):
---
+++ ios_code_generator/tests/test_json2fields.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from StringIO import StringIO
+
+import sys
+
+from ios_code_generator.generators import json_to_fields
+
+__author__ = 'banxi'
+
+def test_json2fields():
+    strio = StringIO(u"""
+                "following": false,
+                "allow_all_act_msg": false,
+                "remark": "",
+                "geo_enabled": true,
+                "verified": false,
+                "allow_all_comment": true
+    """)
+    sys.stdin = strio
+    json_to_fields()
