2c6a8c4e8f053f00031e342eee8432c5e22ba3db
banxi1988 <banxi1988@gmail.com>
2017-03-08 09:04:20 +0800

    优化 自动导入生成器模块

--- ios_code_generator/models/__init__.py
+++ ios_code_generator/models/__init__.py
@@ -3,14 +3,20 @@
 __author__ = 'banxi'
 
 from .core import *
-from .enum_model import EnumModel
-from .data_model import DataModel
-from .view_model import ViewModel
-from .controller_model import ControllerModel
-from .settings_model import SettingsModel
-from .dao_model import DaoModel
-from .router_model import RouterModel, ApiServiceModel
-from .button_group_model import ButtonGroupModel
-from .const_model import  ConstModel
-from .form_model import FormModel
+
+def _auto_import_models():
+    import os
+    import importlib
+    searchpath = os.path.dirname(__file__)
+    exclude_modules = ['__init__', 'core']
+    for filename in os.listdir(searchpath):
+        if not filename.endswith('.py'):
+            continue
+        module_name = filename[:-3]
+        if module_name in exclude_modules:
+            continue
+        importlib.import_module("."+module_name, 'ios_code_generator.models')
+
+# 由于 Model generator 使用装饰器自动注册的机制, 所以需要模块导入之后才能自动注册,所以在这里需要自动导入.
+_auto_import_models()
 
